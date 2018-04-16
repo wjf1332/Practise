f581380209eda88f5026a9c1e85f08a3e6d282a8
wjf <wjf@mail.page>
2018-04-16 11:40:35 +0800

    write a i2c_demo

---
+++ i2c_demo.c
@@ -0,0 +1,34 @@
+#include "demo.h"
+
+void i2c_demo(void)
+{
+#define SIZE		8
+	U8 i = 0;
+	U8 sBuf[SIZE] = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07};
+	U8 rBuf[SIZE];
+
+	Clock_Setting();
+	Led_Init();
+	UART_Init();
+	LED2_ON;
+	I2C_Init(STANDARD_MODE);
+	
+	UART_SendString((U8*)"I2C Demo!\r\n",0);
+	while(1)
+	{
+		Delay(50);
+		I2C_Send(0x60,sBuf,0x08);
+		I2C_Recv(0x61,rBuf,0x08);
+		
+		for(i = 0; i < SIZE; i++)
+		{
+			if(sBuf[i] != rBuf[i])
+			{
+				LED2_OFF;
+				while(1);
+			}
+		}
+		LED1_FLIP;
+		UART_SendHex((U8*)rBuf,0x08);
+	}
+}
